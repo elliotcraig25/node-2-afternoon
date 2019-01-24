abb44abd20b03e7e1dfb848be60fedae7c8a874a
elliot <elliotgcraig@icloud.com>
2019-01-24 15:18:01 -0700

    finished afternoon project

---
+++ db/create_product.sql
@@ -0,0 +1,2 @@
+-- insert into product (name, discription, price, image_url) values ($1, $2, $3, $4);
+INSERT INTO product ( name, description, price, image_url ) VALUES ( $1, $2, $3, $4 );
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+require('dotenv').config();
+const pro_ctrl = require('./products_controller');
+
+const app = express();
+app.use(bodyParser.json());
+
+massive(process.env.CONNECTION_STRING).then((dbInstance)=>{
+    app.set('db', dbInstance)
+}).catch((err)=>
+    console.log(err)
+)
+
+app.post(`/api/products`, pro_ctrl.create)
+
+app.get(`/api/products`, pro_ctrl.getAll)
+
+app.get(`/api/products/:id`, pro_ctrl.getOne)
+
+app.put(`/api/products/:id`, pro_ctrl.update)
+
+app.delete(`/api/products/:id`, pro_ctrl.delete)
+
+port = process.env.PORT || 3000
+app.listen(port, ()=>{console.log(`listening on port ${port}`)}) 
